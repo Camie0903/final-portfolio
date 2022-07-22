2e9bc2337381d3e64c4cbc9304faf2c2a894dcee
Camie0903 <camerontamboer03@gmail.com>
2022-07-22 16:41:05 +0200

    finals

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
-import Footer from ".footer.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
