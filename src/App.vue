73b58ef20d36ab020f40044e8a29539a066748b6
Camie0903 <camerontamboer03@gmail.com>
2023-05-08 16:30:30 +0200

    deploy2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
  <Navbar/>
   <router-view/>
-  <Footer />
+  <!-- <Footer /> -->
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
+// import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Navbar,
-    Footer,
+    // Footer,
   }
 };
 </script>
